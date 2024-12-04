558c1b77d9edb405379c948846e2b53373d6e099
Nathan Ruba <nathan.ruba2003@gmail.com>
2024-12-05 00:10:02 +0100

    LC created

---
+++ risc.srcs/sources_1/new/LC.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/05/2024 12:01:51 AM
+-- Design Name: 
+-- Module Name: LC - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LC is
+    Port ( OP : in STD_LOGIC_VECTOR (7 downto 0);
+           W : out STD_LOGIC);
+end LC;
+
+architecture Behavioral of LC is
+
+begin
+    process(OP)
+    begin
+        if OP = x"08" then
+            W <= '1';
+        else
+            W <= '0';
+        end if;
+    end process;
+end Behavioral;
